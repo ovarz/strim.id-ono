2ff9832ff9b6ca9e30746609744d8a548dc99a9b
ovarz <ovarz_03@yahoo.com>
2022-01-14 22:48:13 +0700

    a

--- inc/base.php
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html lang="id">
-<base href="/strim.id/" />
-<?php $_SERVER['STRIMID'] = $_SERVER['DOCUMENT_ROOT'] . '/strim.id/'; ?>
-<?php $_SERVER['STRIMID_ONO'] = $_SERVER['DOCUMENT_ROOT'] . '/strim.id-ono/'; ?>
--- module/search-box.php
+++
@@ -1,5 +0,0 @@
-  <link rel="stylesheet" type="text/css" href="css/search.css?<?php echo $anticache; ?>"/>
-  <section class="section-container section-search">
-    <input class="search-field" name="" type="text" placeholder="Insert keyword here.....">
-    <button class="search-button btn content_center" onclick="location.href='../strim.id-ono/<?php echo $search_page; ?>';"><span>Search</span></button>
-  </section>
